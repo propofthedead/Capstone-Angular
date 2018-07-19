d2851ffbb37ea4d5d80525bccfa12bcbdefadf37
Foster Stule <camodude5@yahoo.com>
2018-07-19 15:35:56 -0400

    changed file locations

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {User} from './user';
-import {JsonResponse} from './JsonResponse';
+import {JsonResponse} from '../JsonResponse';
 
 @Injectable({
     providedIn:'root'
