32ee27a8ba4a4c58cf3d75bd837337524217df04
Foster Stule <camodude5@yahoo.com>
2018-07-19 15:34:24 -0400

    added user-get

---
+++ src/app/user/user-get/user-get.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-get',
+  templateUrl: './user-get.component.html',
+  styleUrls: ['./user-get.component.css']
+})
+export class UserGetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
