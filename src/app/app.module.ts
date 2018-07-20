573b18c2df3084146dff00776cf1534cd767040d
Foster Stule <camodude5@yahoo.com>
2018-07-20 10:04:25 -0400

    changed to edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
-import { UserChangeComponent } from './user/user-change/user-change.component';
 import { UserDeleteComponent } from './user/user-delete/user-delete.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
