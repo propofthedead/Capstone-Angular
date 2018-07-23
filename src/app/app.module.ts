cb921290261f115b11bd1fde164134382d81c62d
Foster Stule <camodude5@yahoo.com>
2018-07-23 16:58:13 -0400

    finally fixed ngmodel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +42,11 @@ import { UserLoginComponent } from './user/user-login/user-login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
