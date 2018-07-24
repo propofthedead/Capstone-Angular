f4d314573b55db6ff902edf6b583269b1c3d2105
Foster Stule <camodude5@yahoo.com>
2018-07-24 14:39:25 -0400

    added vendor class and methods

--- src/app/vendor/vendor.ts
+++ src/app/vendor/vendor.ts
@@ -1,2 +1,19 @@
 export class Vendor {
+    Id: number;
+    Code: string;
+    Name: string;
+    Address: string;
+    City: string;
+    State: string;
+    Zipcode: string;
+    Email: string;
+    Phone: string;
+    IsPreApproved: boolean;
+    Active: boolean;
+
+    constructor(){
+        this.Id=0;
+        this.IsPreApproved=false;
+        this.Active=true;
+    }
 }
