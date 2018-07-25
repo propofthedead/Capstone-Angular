dc81896a5ea110efc47d405ced565dcd294152a7
Foster Stule <camodude5@yahoo.com>
2018-07-25 09:14:21 -0400

    made a login method tied to a format

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '@user/user.service';
+import { Route, Router } from '../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  password: string;
+
+  login():void{
+    this.Userscv.authenticate(this.username,this.password)
+      .subscribe(resp=>{
+        console.log(resp);
+        this.route.navigateByUrl('home');
+      })
+    
+  }
+
+
+  constructor(private Userscv: UserService, private route: Router) { }
 
   ngOnInit() {
   }
