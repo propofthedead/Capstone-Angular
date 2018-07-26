496d3a44dd6ead16adcf07a25c5efa7831a16b0b
Foster Stule <camodude5@yahoo.com>
2018-07-26 10:45:53 -0400

    added request service methods

--- src/app/request/request.service.ts
+++ src/app/request/request.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { JsonResponse } from '../JsonResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
 
-  constructor() { }
+  url="http://localhost:55941/PurchaseRequests/"
+
+  list(): Observable<JsonResponse>{
+    return this.http.get(this.url+"list") as Observable<JsonResponse>;
+  }
+
+  get(id): Observable<JsonResponse>{
+    return this.http.get(this.url+"get/"+id) as Observable<JsonResponse>;
+  }
+
+  edit(request: Request): Observable<JsonResponse>{
+    return this.http.post(this.url+"edit",request) as Observable<JsonResponse>;
+  }
+
+  create(request:Request): Observable<JsonResponse>{
+    return this.http.post(this.url+"create",request) as Observable<JsonResponse>;
+  }
+
+  remove(request:Request): Observable<JsonResponse>{
+    return this.http.post(this.url+"delete",request) as Observable<JsonResponse>;
+  }
+
+  constructor(private http: HttpClient) { }
 }
--- src/app/request/request.ts
+++ src/app/request/request.ts
@@ -1,2 +1,13 @@
+import { User } from "@user/user";
+
 export class Request {
+    Id:number;
+    Description:string;
+    Justification:string;
+    RejectReason:string;
+    DeliviryMode:string;
+    Status:string;
+    Price:number;
+    User: User;
+    UserId: number;
 }
