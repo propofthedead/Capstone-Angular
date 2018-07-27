ef356143f681b3931cf2c545773d1a6f93a09c05
Foster Stule <camodude5@yahoo.com>
2018-07-27 09:49:38 -0400

    added functionaility to the line edit component

--- src/app/lines/lines-edit/lines-edit.component.ts
+++ src/app/lines/lines-edit/lines-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LinesService } from '../lines.service';
+import { Router, ActivatedRoute } from '../../../../node_modules/@angular/router';
+import { Lines } from '../lines';
 
 @Component({
   selector: 'app-lines-edit',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lines-edit.component.css']
 })
 export class LinesEditComponent implements OnInit {
+  line:Lines;
 
-  constructor() { }
+  edit():void{
+    this.Linesvc.edit(this.line)
+    .subscribe(resp=>{
+      console.log(resp);
+    })
+    this.router.navigateByUrl('/line/list');
+  }
+
+  constructor(private Linesvc:LinesService, private router:Router, private routed:ActivatedRoute) { }
 
   ngOnInit() {
+    let id= this.routed.snapshot.params.id;
+    this.Linesvc.get(id)
+    .subscribe(resp=>{
+        this.line=resp.Data;
+        console.log(resp);
+    })
   }
 
 }
