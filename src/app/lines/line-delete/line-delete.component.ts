133317172e9bc1155065ccd7bcc9f7960a643da9
Foster Stule <camodude5@yahoo.com>
2018-07-27 12:27:56 -0400

    added functionality

--- src/app/lines/line-delete/line-delete.component.ts
+++ src/app/lines/line-delete/line-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LinesService } from '../lines.service';
+import { ActivatedRoute, Router } from '../../../../node_modules/@angular/router';
+import { Lines } from '../lines';
 
 @Component({
   selector: 'app-line-delete',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-delete.component.css']
 })
 export class LineDeleteComponent implements OnInit {
+line:Lines
+  constructor(private LineSvc:LinesService, private routed: ActivatedRoute, private router:Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() 
+  {
+    let id= this.routed.snapshot.params.id;
+    this.LineSvc.get(id)
+    .subscribe(resp=>{
+      this.line=resp.Data;
+      console.log(resp);
+    })
   }
 
 }
