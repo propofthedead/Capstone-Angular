7e5dbe85d0b23835f0e79663bc449de8c950b51e
Foster Stule <camodude5@yahoo.com>
2018-07-31 10:51:55 -0400

    added a is admin check

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -3,6 +3,7 @@ import {UserService} from '@user/user.service';
 import {User} from '@user/user';
 import {JsonResponse} from '../../JsonResponse';
 import { Router, ActivatedRoute } from '@angular/router';
+import { SystemService } from '../../system/system.service';
 
 
 @Component({
@@ -13,20 +14,38 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class UserEditComponent implements OnInit {
 
   user: User= new User();
-  
+  logged: User= null;
+  verified: boolean=false;
 
   edit():void{
     console.log(this.user);
+    this.verified=this.able();
+    if(this.verified==true){
     this.Usersvc.edit(this.user)
       .subscribe(resp=>{
         console.log(resp);
         this.route.navigateByUrl('/user/list');
       })
+    }
   }
 
-  constructor(private Usersvc: UserService, private route:Router, private routed: ActivatedRoute) { }
+  able(): boolean{
+    if(this.logged.Id===this.user.Id){
+      return true;
+    }
+    if(this.logged.IsAdmin===true){
+      return true;
+    }
+    return false;
+  }
+
+  constructor(private Usersvc: UserService, private route:Router, private routed: ActivatedRoute, private Syssvc:SystemService) { }
 
   ngOnInit() {
+    if(!this.Syssvc.checkLogin()){
+      this.route.navigateByUrl('/user/login')
+    }
+    this.logged=this.Syssvc.getLoggedInUser();
     let id= this.routed.snapshot.params.id;
     this.Usersvc.get(id)
       .subscribe(resp=>{
