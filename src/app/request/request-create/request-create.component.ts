6b04206a96bc648896df0be2bf959ee6043d2b9b
Foster Stule <camodude5@yahoo.com>
2018-07-31 17:04:35 -0400

    did some editing

--- src/app/request/request-create/request-create.component.ts
+++ src/app/request/request-create/request-create.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RequestService } from '../request.service';
 import { Router } from '../../../../node_modules/@angular/router';
 import { Request } from '../request';
+import { SystemService } from '../../system/system.service';
+import { User } from '@user/user';
 
 @Component({
   selector: 'app-request-create',
@@ -10,19 +12,23 @@ import { Request } from '../request';
 })
 export class RequestCreateComponent implements OnInit {
 request: Request;
+logged: User;
 
 
 
 create():void{
+  this.request.UserId=this.logged.Id;
   this.Requestsvc.create(this.request)
   .subscribe(resp=>{
     console.log(resp);
   })
   this.router.navigateByUrl("/request/list");
 }
-  constructor(private Requestsvc:RequestService, private router: Router) { }
+  constructor(private Requestsvc:RequestService, private router: Router, private Syssvc:SystemService) { }
 
   ngOnInit() {
+    this.Syssvc.checkLogin();
+    this.logged=this.Syssvc.getLoggedInUser();
   }
 
 }
