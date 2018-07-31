fe44b325e190b77b59428fe519cbd32363d8da6c
Foster Stule <camodude5@yahoo.com>
2018-07-31 10:51:44 -0400

    added an is admin check

--- src/app/user/user-delete/user-delete.component.ts
+++ src/app/user/user-delete/user-delete.component.ts
@@ -3,6 +3,7 @@ import {UserService} from '@user/user.service';
 import {User} from '@user/user';
 import {JsonResponse} from '../../JsonResponse';
 import { Router, ActivatedRoute } from '@angular/router';
+import { SystemService } from '../../system/system.service';
 
 @Component({
   selector: 'app-user-delete',
@@ -12,21 +13,34 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class UserDeleteComponent implements OnInit {
 
   user: User= new User();
+  logged: User=null;
+  verified: boolean=false;
   delete():void{
     console.log(this.user);
+    this.verified=this.able();
+    if(this.verified==true){
     this.Usersvc.remove(this.user)
       .subscribe(resp=>{
         this.router.navigateByUrl('/user/list');
         console.log(resp);
       })
+    }
   }
-
+  able():boolean{
+    if(this.logged.IsAdmin==true){
+      return true;
+    }
+    return false;
+  }
   back():void{
     this.router.navigateByUrl('/user/list');
   }
-  constructor(private router:Router, private Usersvc:UserService, private routed:ActivatedRoute) { }
+  constructor(private router:Router, private Usersvc:UserService, private routed:ActivatedRoute, private Syssvc:SystemService) { }
 
   ngOnInit() {
+    this.Syssvc.checkLogin();
+    this.logged=this.Syssvc.getLoggedInUser();
+
     let id= this.routed.snapshot.params.id;
     this.Usersvc.get(id)
       .subscribe(resp=>{
