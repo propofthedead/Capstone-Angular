07def6d9b649b7f638e3523d1aed64cff7c7ff9c
Foster Stule <camodude5@yahoo.com>
2018-07-31 12:05:29 -0400

    added user checking

--- src/app/request/request-edit/request-edit.component.ts
+++ src/app/request/request-edit/request-edit.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RequestService } from '../request.service';
 import { Router, ActivatedRoute } from '../../../../node_modules/@angular/router';
 import { Request } from '../request';
+import { SystemService } from '../../system/system.service';
+import { User } from '@user/user';
 
 @Component({
   selector: 'app-request-edit',
@@ -10,16 +12,18 @@ import { Request } from '../request';
 })
 export class RequestEditComponent implements OnInit {
 
-  request: Request
-
-  edit():void{
+  request: Request;
+  logged: User;
+  
+   
+    edit():void{
     this.Requestsvc.edit(this.request)
     .subscribe(resp=>{
       console.log(resp);
     })
     this.router.navigateByUrl('/request/list');
   }
-  constructor(private Requestsvc: RequestService, private router:Router, private routed: ActivatedRoute) { }
+  constructor(private Requestsvc: RequestService, private router:Router, private routed: ActivatedRoute, private Syssvc: SystemService) { }
 
   ngOnInit() {
     let id= this.routed.snapshot.params.id;
@@ -28,6 +32,11 @@ export class RequestEditComponent implements OnInit {
       this.request= resp.Data;
       console.log(resp);
     })
+    this.Syssvc.checkLogin();
+    this.logged=this.Syssvc.getLoggedInUser();
+    if(!(this.logged.IsAdmin ==true|| this.logged.IsReviewer==true|| this.logged.Id==this.request.UserId)){
+      this.router.navigateByUrl('/request/list');
+    }
   }
 
 }
