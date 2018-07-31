ba76d5bfdbb9c58d3c3741828132930a2db54c7c
Foster Stule <camodude5@yahoo.com>
2018-07-31 11:14:32 -0400

    added a admin check

--- src/app/vendor/vendor-delete/vendor-delete.component.ts
+++ src/app/vendor/vendor-delete/vendor-delete.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '../../../../node_modules/@angular/router';
 import { VendorService } from '../vendor.service';
 import { Vendor } from '../vendor';
+import { SystemService } from '../../system/system.service';
+import { User } from '@user/user';
 
 @Component({
   selector: 'app-vendor-delete',
@@ -11,21 +13,34 @@ import { Vendor } from '../vendor';
 export class VendorDeleteComponent implements OnInit {
 
   vendor: Vendor= new Vendor()
+  logged: User=null;
+  verified: boolean=false;
   delete():void{
     console.log(this.vendor);
+    this.verified=this.able();
+    if(this.verified==true){
     this.Vendorsvc.remove(this.vendor)
       .subscribe(resp=>{
         this.route.navigateByUrl('/vendor/list');
         console.log(resp);
       })
     }
-
+    this.back();
+    }
+    able(): boolean{
+      if(this.logged.IsAdmin){
+        return true;
+      }
+      return false;
+    }
     back():void{
       this.route.navigateByUrl('/vendor/list');
     }
-  constructor(private route:Router, private Vendorsvc:VendorService, private routed:ActivatedRoute) { }
+  constructor(private route:Router, private Vendorsvc:VendorService, private routed:ActivatedRoute, private Syssvc: SystemService) { }
 
   ngOnInit() {
+    this.Syssvc.checkLogin();
+    this.logged=this.Syssvc.getLoggedInUser();
     let id= this.routed.snapshot.params.id;
     this.Vendorsvc.get(id)
       .subscribe(resp=>{
