62737325f74051120d3856b08dc60bee6bd8f5f4
Foster Stule <camodude5@yahoo.com>
2018-07-31 10:51:19 -0400

    added a is admin check

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -3,6 +3,7 @@ import {UserService} from '@user/user.service';
 import {User} from '@user/user';
 import {JsonResponse} from '../../JsonResponse';
 import { Router } from '@angular/router';
+import { SystemService } from '../../system/system.service';
 
 
 
@@ -14,19 +15,32 @@ import { Router } from '@angular/router';
 export class UserCreateComponent implements OnInit {
 
  user: User= new User();
-
+ logged: User=null;
+ verified: boolean=false;
   create(): void{
     console.log(this.user);
+    this.verified=this.able();
+    if(this.verified==true){
     this.Usersvc.create(this.user)
       .subscribe(resp=>{
         this.router.navigateByUrl('/user/list');
         console.log(resp);
       })
+    }
   }
 
-  constructor(private Usersvc: UserService, private router: Router) { }
+  able():boolean{
+    if(this.logged.IsAdmin==true){
+      return true;
+    }
+    return false;
+  }
+
+  constructor(private Usersvc: UserService, private router: Router, private Syssvc: SystemService) { }
 
   ngOnInit() {
+    this.Syssvc.checkLogin();
+    this.logged=this.Syssvc.getLoggedInUser();
   }
 
 }
